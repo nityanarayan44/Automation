6d586ae3a1ad0b7833fde4346d048c7c2fcdc9a6
nityanarayan44 <nityanarayan44@live.com>
2017-12-01 14:07:21 +0530

    Code has been updated

--- WithSelenium/WebAutomation/OnPython_WithBehave/Src/steps/Steps_For_B.py
+++ WithSelenium/WebAutomation/OnPython_WithBehave/Src/steps/Steps_For_B.py
@@ -6,15 +6,14 @@ from behave   import given, when, then
 
 #Class Definitions...
 class Steps_For_B:
-    # Global Object..
-    things = "apple"
+    # Global Object (if any)..
 
     @given('there is a "{thing}"')
-    def step_given_assert_thing(self, context, thing):
-        if self.things == thing:
+    def step_given_assert_thing(context, thing):
+        if "apple" == thing:
             print('thing matched.')
 
     @then('it should also match with "{other_thing}"')
-    def step_then_should_match_with(self, context, other_thing):
-        if self.things == other_thing:
+    def step_then_should_match_with(context, other_thing):
+        if "apple" == other_thing:
             print('other_thing matched.')
